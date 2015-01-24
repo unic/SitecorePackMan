cd9e1fd9d08d8e21fe2d32b96a4f5b0df13df97a
Tobias Studer <tobias.studer@unic.com>
2015-01-24 05:47:53 +0100

    Add configuration service to check if an item is included in the config

---
+++ src/Unic.PackMan.Core/Configuration/ConfigurationService.cs
@@ -0,0 +1,12 @@
+﻿namespace Unic.PackMan.Core.Configuration
+{
+    using Sitecore.Data.Items;
+
+    public class ConfigurationService : IConfigurationService
+    {
+        public bool IsItemIncluded(Item item)
+        {
+            return true;
+        }
+    }
+}
