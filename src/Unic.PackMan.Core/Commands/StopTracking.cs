e191ba8f01693f17eb5930b8dd5cbd4f53b62553
Pascal Mathys <pascal.mathys@unic.com>
2015-01-24 04:42:25 +0100

    Add content editor ribbon command skeletons

---
+++ src/Unic.PackMan.Core/Commands/StopTracking.cs
@@ -0,0 +1,18 @@
+﻿namespace Unic.PackMan.Core.Commands
+{
+    using Sitecore.Shell.Framework.Commands;
+
+    /// <summary>
+    /// Command to stop a tracking session
+    /// </summary>
+    public class StopTracking : Command
+    {
+        /// <summary>
+        /// Executes the command
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public override void Execute(CommandContext context)
+        {
+        }
+    }
+}
