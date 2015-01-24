c8c6cb4b4973f0e965aae99a75d04f25d70b3f04
Kevin Brechbühl <kevin.brechbuehl@unic.com>
2015-01-24 11:25:53 +0100

    Change package generation behavior that package can only be generated while tracking is active.

--- src/Unic.PackMan.Core/Commands/GeneratePackage.cs
+++ src/Unic.PackMan.Core/Commands/GeneratePackage.cs
@@ -6,12 +6,35 @@
     using Sitecore.Shell.Framework.Commands;
     using Sitecore.Web.UI.Sheer;
     using Unic.PackMan.Core.Pipelines.GeneratePackage;
+    using Unic.PackMan.Core.User;
 
     /// <summary>
     /// Command for generating a item package based on the last session
     /// </summary>
     public class GeneratePackage : Command
     {
+        /// <summary>
+        /// The user service
+        /// </summary>
+        private readonly IUserService userService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GeneratePackage"/> class.
+        /// </summary>
+        public GeneratePackage() : this(new UserService())
+        {
+            
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GeneratePackage"/> class.
+        /// </summary>
+        /// <param name="userService">The user service.</param>
+        public GeneratePackage(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
         /// <summary>
         /// Executes the command
         /// </summary>
@@ -20,6 +43,21 @@
         {
             var parameters = new NameValueCollection();
             Context.ClientPage.Start(this, "Run", parameters);
+        }
+
+        /// <summary>
+        /// Queries the state.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <returns>Command state for this command.</returns>
+        public override CommandState QueryState(CommandContext context)
+        {
+            if (!this.userService.IsTrackingEnabled())
+            {
+                return CommandState.Disabled;
+            }
+
+            return base.QueryState(context);
         }
 
         /// <summary>
