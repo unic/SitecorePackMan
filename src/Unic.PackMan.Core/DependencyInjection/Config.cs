6a4880cceddfff4674cef6714ede1f6b57e21557
Kevin Brechbühl <kevin.brechbuehl@unic.com>
2015-01-24 04:30:11 +0100

    Install Ninject.

---
+++ src/Unic.PackMan.Core/DependencyInjection/Config.cs
@@ -0,0 +1,18 @@
+﻿namespace Unic.PackMan.Core.DependencyInjection
+{
+    using Ninject.Modules;
+
+    /// <summary>
+    /// Ninject module config.
+    /// </summary>
+    public class Config : NinjectModule
+    {
+        /// <summary>
+        /// Loads the module into the kernel.
+        /// </summary>
+        public override void Load()
+        {
+            
+        }
+    }
+}
---
+++ src/Unic.PackMan.Core/DependencyInjection/DependencyResolver.cs
@@ -0,0 +1,35 @@
+﻿namespace Unic.PackMan.Core.DependencyInjection
+{
+    using Ninject;
+    using Ninject.Modules;
+
+    /// <summary>
+    /// The dependency resolver for Ninject.
+    /// </summary>
+    public static class DependencyResolver
+    {
+        /// <summary>
+        /// The kernel
+        /// </summary>
+        private static IKernel kernel;
+
+        /// <summary>
+        /// Creates the kernel.
+        /// </summary>
+        /// <param name="modules">The modules.</param>
+        public static void CreateKernel(INinjectModule[] modules)
+        {
+            kernel = new StandardKernel(modules);
+        }
+
+        /// <summary>
+        /// Resolves s specific instance.
+        /// </summary>
+        /// <typeparam name="T">Type to resolve</typeparam>
+        /// <returns>Resolved instance</returns>
+        public static T Resolve<T>()
+        {
+            return kernel.Get<T>();
+        }
+    }
+}
