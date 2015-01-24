ce89105f3f730d32fafb20673ba6be4c77c89930
Kevin Brechbühl <kevin.brechbuehl@unic.com>
2015-01-24 07:23:05 +0100

    Add pipeline processor to clear the tracking list when start tracking.

---
+++ src/Unic.PackMan.Core/Pipelines/StartTracking/ClearTrackingList.cs
@@ -0,0 +1,20 @@
+﻿namespace Unic.PackMan.Core.Pipelines.StartTracking
+{
+    using Sitecore.Pipelines;
+    using Unic.PackMan.Core.User;
+
+    public class ClearTrackingList
+    {
+        private readonly IUserService userService;
+
+        public ClearTrackingList(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public void Process(PipelineArgs args)
+        {
+            this.userService.SaveTrackingList(string.Empty);
+        }
+    }
+}
