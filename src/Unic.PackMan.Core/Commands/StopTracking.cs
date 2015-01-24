21ec2a6882382c587bb62d13e6b8ec8380cf3e4c
Kevin Brechbühl <kevin.brechbuehl@unic.com>
2015-01-24 06:36:05 +0100

    Add poor man dependency injection to commands... Damn...

--- src/Unic.PackMan.Core/Commands/StopTracking.cs
+++ src/Unic.PackMan.Core/Commands/StopTracking.cs
@@ -10,6 +10,29 @@
     public class StopTracking : Command
     {
         /// <summary>
+        /// The user service
+        /// </summary>
+        private readonly IUserService userService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StopTracking"/> class.
+        /// HINT: Use poor man dependency injection here, because the Sitecore factory does only seem to work for
+        /// events and pipelines. Dear Sitecore devs, why??
+        /// </summary>
+        public StopTracking() : this(new UserService())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StopTracking"/> class.
+        /// </summary>
+        /// <param name="userService">The user service.</param>
+        public StopTracking(IUserService userService)
+        {
+            this.userService = userService;
+        }
+        
+        /// <summary>
         /// Executes the command
         /// </summary>
         /// <param name="context">The context.</param>
@@ -26,8 +49,7 @@
         /// <returns>State of the current command</returns>
         public override CommandState QueryState(CommandContext context)
         {
-            var service = new UserService();
-            if (!service.IsTrackingEnabled())
+            if (!this.userService.IsTrackingEnabled())
             {
                 return CommandState.Disabled;
             }
